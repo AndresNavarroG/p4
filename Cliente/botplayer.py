15dcedf16fe31a9a028565d105659d3cc935b0e0
AndresNavarroG <andres.blarep.ope@gmail.com>
2016-10-06 21:12:35 -0300

    PRIMER INTENTO

--- Cliente/botplayer.py
+++ Cliente/botplayer.py
@@ -1,20 +1,94 @@
 import numpy
-
-def bot(amenaza):
-    pass
-
-def checkear_amenazas(amenazas):
-    a, b = amenazas.strip().split(":")
-    print a, b
-    
-
+import random 
+#ocupar posicion
+#TIENE MUCHOS ERRORESSS
 def escoger_movimiento( amenazas ):
-    checkear_amenazas(amenazas)
-    movimiento_x = "2"
-    movimiento_y = "0" 
+    movimiento_x=""
+    movimiento_y=""
+    amenazas,cuadrantes=amenazas.strip().split(":")
+    amenazas=amenazas.strip().split("-")
+    cuadrantes=cuadrantes.strip().split("-")
+    cuadrante_seguro=0
+    n_de_naves=1000
+    for cuadrante in cuadrantes:
+        if int(cuadrante) < n_de_naves:
+            cuadrante_seguro=cuadrante.index(cuadrante)
+            n_de_naves=int(cuadrante)
+    suma_vertical=int(cuadrantes[0])+int(cuadrantes[2])
+    suma_horizontal=int(cuadrantes[1])+int(cuadrantes[3])
+
+    g1=amenazas.count("1")
+    g2=amenazas.count("2")
+    g3=amenazas.count("3")
+    if g1 <= g2 and g1 <=g3:
+        eleccion = random.randint(4,5)
+    if g2 <= g1 and g2 <=g3:
+        eleccion = random.randint(2,3)
+    if g3 <= g1 and g3 <=g2:
+        eleccion = 1
+    if g1==g2 and g2==g3 and g3==0:
+        eleccion = random.randint(0,1)
+        if eleccion==0:
+            eleccion=random.randint(-3,-1)
+        if eleccion==1:
+            eleccion=random.randint(1,3)
+    if suma_vertical > suma_horizontal:
+        if cuadrante_seguro==0:
+            movimiento_x='0'
+            movimiento_y=str(eleccion)
+        if cuadrante_seguro==2:
+            movimiento_x='0'
+            movimiento_y=str(-eleccion)
+    if suma_vertical < suma_horizontal:
+        if cuadrante_seguro==1:
+            movimiento_x=str(-eleccion)
+            movimiento_y='0'
+        if cuadrante_seguro==3:
+            movimiento_x=str(eleccion)
+            movimiento_y="0"
+
+    #ir renovando posicion
     return movimiento_x + "," + movimiento_y
 
 def escoger_disparo( amenazas ):
-    disparo_x = "0"
-    disparo_y = "1"
+    disparo_x=""
+    disparo_y=""
+    amenazas,cuadrantes=amenazas.strip().split(":")
+    amenazas=amenazas.strip().split("-")
+    cuadrantes=cuadrantes.strip().split("-")
+    cuadrante_peligroso=0
+    n_de_naves=0
+    for cuadrante in cuadrantes:        
+        if int(cuadrante) > n_de_naves:
+            cuadrante_peligroso=cuadrantes.index(cuadrante)
+            n_de_naves=int(cuadrante)
+    
+    g1=amenazas.count("1")
+    g2=amenazas.count("2")
+    g3=amenazas.count("3")
+    if g1 >= g2 and g1 >=g3:
+        eleccion = random.randint(4,5)
+    if g2 >= g1 and g2 >=g3:
+        eleccion = random.randint(2,3)
+    if g3 >= g1 and g3 >=g2:
+        eleccion = 1
+    if g1==g2 and g2==g3 and g3==0:
+        eleccion = random.randint(0,1)
+        if eleccion==0:
+            eleccion=random.randint(-5,-1)
+        if eleccion==1:
+            eleccion=random.randint(1,5)
+    if cuadrante_peligroso==0:
+        disparo_x="0"
+        disparo_y=str(eleccion)
+    if cuadrante_peligroso==1:
+        disparo_x=str(-eleccion)
+        disparo_y="0"
+    if cuadrante_peligroso==2:
+        disparo_x="0"
+        disparo_y=str(-eleccion)
+    if cuadrante_peligroso==3:
+        disparo_x=str(eleccion)
+        disparo_y="0"
     return disparo_x + "," + disparo_y
+
