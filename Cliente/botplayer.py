fb69586554642cf6d83039187d4e5ebc69a17709
basfom <jose.follert@sansano.usm.cl>
2016-10-07 10:39:34 -0300

    actualizacion

--- Cliente/botplayer.py
+++ Cliente/botplayer.py
@@ -1,7 +1,6 @@
 import numpy
 import random 
 #ocupar posicion
-#TIENE MUCHOS ERRORESSS
 def escoger_movimiento( amenazas ):
     movimiento_x=""
     movimiento_y=""
@@ -21,25 +20,28 @@ def escoger_movimiento( amenazas ):
     g2=amenazas.count("2")
     g3=amenazas.count("3")
     if g1 <= g2 and g1 <=g3:
-        eleccion = random.randint(4,5)
-    if g2 <= g1 and g2 <=g3:
+        if g2 <= g1 and g2 <=g3:
+            eleccion = random.randint(2,3)
+        if g3 <= g1 and g3 <=g2:
+            eleccion = 1
+    elif g2 <= g1 and g2 <=g3:
         eleccion = random.randint(2,3)
-    if g3 <= g1 and g3 <=g2:
+    elif g3 <= g1 and g3 <=g2:
         eleccion = 1
-    if g1==g2 and g2==g3 and g3==0:
+    elif g1==g2 and g2==g3 and g3==0:
         eleccion = random.randint(0,1)
         if eleccion==0:
             eleccion=random.randint(-3,-1)
         if eleccion==1:
             eleccion=random.randint(1,3)
-    if suma_vertical > suma_horizontal:
+    if suma_vertical < suma_horizontal:
         if cuadrante_seguro==0:
             movimiento_x='0'
             movimiento_y=str(eleccion)
         if cuadrante_seguro==2:
             movimiento_x='0'
             movimiento_y=str(-eleccion)
-    if suma_vertical < suma_horizontal:
+    if suma_vertical > suma_horizontal:
         if cuadrante_seguro==1:
             movimiento_x=str(-eleccion)
             movimiento_y='0'
@@ -48,9 +50,11 @@ def escoger_movimiento( amenazas ):
             movimiento_y="0"
 
     #ir renovando posicion
+    print  movimiento_x + "," + movimiento_y
     return movimiento_x + "," + movimiento_y
 
 def escoger_disparo( amenazas ):
+    print "comenzando disparo"
     disparo_x=""
     disparo_y=""
     amenazas,cuadrantes=amenazas.strip().split(":")
@@ -66,6 +70,7 @@ def escoger_disparo( amenazas ):
     g1=amenazas.count("1")
     g2=amenazas.count("2")
     g3=amenazas.count("3")
+    print "amenazas"
     if g1 >= g2 and g1 >=g3:
         eleccion = random.randint(4,5)
     if g2 >= g1 and g2 >=g3:
@@ -90,5 +95,5 @@ def escoger_disparo( amenazas ):
     if cuadrante_peligroso==3:
         disparo_x=str(eleccion)
         disparo_y="0"
+    print  disparo_x + "," + disparo_y
     return disparo_x + "," + disparo_y
-
