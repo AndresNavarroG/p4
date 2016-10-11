f395db729616c5169f00ff345ed7839b3a1d770e
AndresNavarroG <andres.blarep.ope@gmail.com>
2016-10-11 05:02:08 -0300

    cambiado

--- Cliente/cliente_alumnos.py
+++ Cliente/cliente_alumnos.py
@@ -23,9 +23,6 @@ try:
 
         #Recibir mensaje del servidor
         mensaje = cliente.recv(1024)
-        if len(mensaje.split(':')) != 2:
-            break
-
 
         # mensaje, es el string de amenazas, en base a aquel mensaje, tomar una decision
         
@@ -35,9 +32,19 @@ try:
         escoger_movimieto( mensaje ) se encuentra en botdummy, es tarea de ustedes completar esta funcion.
 
         """
-        disparo = escoger_disparo( mensaje )        
-        movimiento = escoger_movimiento( mensaje )
-        mensaje =  disparo+"/"+movimiento
+        posicion=list(posicion) #se transforma la tupla a una lista para poder modificarla
+        #disparo = escoger_disparo( mensaje )        
+        #movimiento = escoger_movimiento( mensaje )
+        #mensaje =  disparo+"/"+movimiento
+        mensaje=procesar(mensaje,posicion)
+        mov=mensaje.split("/")[1]
+        sumar_pos=mov.split(",")
+        posicion[0]+=int(sumar_pos[0])
+        posicion[1]+=int(sumar_pos[1])
+        posicion[0]=posicion[0]%11
+        posicion[1]=posicion[1]%11
+        
+        
         cliente.send(mensaje )
 except socket.error:
     cliente.close()
